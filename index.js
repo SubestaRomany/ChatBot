f028edd99830bd466039445f830bfe6555aa3bc0
unknown <you@example.com>
2025-07-12 02:09:00 +0300

    v4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const axios = require("axios");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("./creds.json");
+const creds = JSON.parse(process.env.GOOGLE_CREDS);
+
 
 const app = express();
 app.use(express.json());
